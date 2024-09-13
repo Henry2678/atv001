c2a2443d124c7b6959392e135f65405e6f354c48
Henry2678 <soldadohuefl@gmail.com>
2024-09-13 09:02:09 -0300

    Atualizar o atividade1.py

--- atividade1.py
+++ atividade1.py
@@ -1,5 +1,5 @@
-# Crie um programa que receba dois números do usuário e exiba a soma desses números1
-valor = int(input("digite um numero "))
-valor2 = int(input("digite outro numero "))
+# Crie um programa que receba dois números do usuário e exiba a soma desses números 1
+valor = int(input('digite um numero:'))
+valor2 = int(input('digite outro numero '))
 soma = valor + valor2
-print(f"a soma é {valor} + {valor2} = {soma} ")
+print(f'a soma é {valor} + {valor2} = {soma}')
